ccb028b77fc1daeba74e573cc5be31946f4f61b1
alphamiracle <mayda.werfali@gmail.com>
2020-05-21 17:51:30 +0200

    added changes to the product controller and the product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,18 +10,18 @@ class Product extends Model
     // Attribute Mutaion
     function getImageAttribute($value)
     {
-        echo 'getImage: ' .  $value;
+        echo 'getImage: ' .  $value . '\n';
         return url($value);
     }
 
     function getCreatedAtAttribute($value)
     {
-        echo 'getCreatedAt' . $value;
+        echo 'getCreatedAt' . $value . '\n';
         return Carbon::parse($value)->diffForHumans();
     }
     function getUpdatedAtAttribute($s)
     {
-        echo 'getUpdatedAt' . $s;
-        return Carbon::parse($s);
+        echo 'getUpdatedAt' . $s . '\n';
+        return Carbon::parse($s)->diffForHumans();
     }
 }
