bc9590786a7eb3ce14b25404a38bfa3c1351c5da
mayda-mac <mayda.werfali@gmail.com>
2020-05-25 22:56:11 +0200

    added mutators to the Ad model

--- app/Models/Ad.php
+++ app/Models/Ad.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ad extends Model
 {
-    //
+     function getImageAttribute($value)
+     {
+         return url($value);
+     }
+ 
+     function getCreatedAtAttribute($value)
+     {
+         return Carbon::parse($value)->diffForHumans();
+     }
+     function getUpdatedAtAttribute($s)
+     {
+         return Carbon::parse($s)->diffForHumans();
+     }
 }
